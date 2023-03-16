14478f81b2f2cb460fe963212b7b807a7cf0d6ea
magdyHakam <magdy.hakam@gmail.com>
2023-03-16 09:27:17 +0200

    clean views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,13 +5,8 @@
 
         <title>Checkout example for Bootstrap</title>
 
-
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-        <!-- Custom styles for this template -->
-        <!-- <link href="./Checkout example for Bootstrap_files/form-validation.css" rel="stylesheet"> -->
     </head>
 
   <body class="bg-light">
@@ -38,9 +33,6 @@
             <li class="nav-item">
               <a class="nav-link" href="/statistics">Statistics</a>
             </li>
-            <!-- <li class="nav-item">
-              <a class="nav-link" href="#">Profile</a>
-            </li> -->
           </ul>
         </div>
       </div>
@@ -65,14 +57,5 @@
         @yield('content')
 
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./Checkout example for Bootstrap_files/jquery-3.3.1.slim.min.js.download" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="./Checkout example for Bootstrap_files/popper.min.js.download"></script>
-    <script src="./Checkout example for Bootstrap_files/bootstrap.min.js.download"></script>
-    <script src="./Checkout example for Bootstrap_files/holder.min.js.download"></script>
 
 </body></html>
